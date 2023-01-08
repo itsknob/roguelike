5c6c652549b584628ce701eff5ea01b2ad2bc941
Knob <itsknob@gmail.com>
2023-01-08 12:52:08 -0500

    feat: finished chapter 6

--- src/components.rs
+++ src/components.rs
@@ -7,3 +7,6 @@ pub struct Viewshed {
     pub range: i32,
     pub dirty: bool
 }
+
+#[derive(Component, Debug)]
+pub struct Monster {}
--- src/player.rs
+++ src/player.rs
@@ -2,9 +2,9 @@
  * Player Logic
  */
 
-use rltk::{VirtualKeyCode, Rltk};
+use rltk::{VirtualKeyCode, Point, Rltk};
 use specs::prelude::*;
-use super::{Position, Player, State, Map, Viewshed};
+use super::{Position, Player, State, Map, Viewshed, RunState};
 use crate::map::{TileType};
 use std::cmp::{min, max}; 
 
@@ -19,16 +19,20 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
         if map.tiles[destination_idx] != TileType::Wall {
             pos.x = min(79 , max(0, pos.x + delta_x));
             pos.y = min(49 , max(0, pos.y + delta_y));
+    
+            let mut ppos = ecs.write_resource::<Point>();
+            ppos.x = pos.x;
+            ppos.y = pos.y;
 
             viewshed.dirty = true;
         }
     }
 }
 
-pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
     // Player movement
     match ctx.key {
-        None => {} // Nothing Happned
+        None => { return RunState::Paused } // Nothing Happned
         Some(key) => match key {
             VirtualKeyCode::Left |
             VirtualKeyCode::Numpad4 |
@@ -46,8 +50,9 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
             VirtualKeyCode::Numpad2 |
             VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
 
-            _ => {}
+            _ => { return RunState::Paused }
         },
     }
+    RunState::Running
 }
 
